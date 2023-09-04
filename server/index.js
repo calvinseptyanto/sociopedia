5f93d4c2528cc4d42ba0f4269b036bcf5a7646be
calvinseptyanto <98633109+calvinseptyanto@users.noreply.github.com>
2023-09-04 15:32:54 +0800

    Added post routes: finished backend

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
 import userRoutes from "./routes/users.js";
 
 /* CONFIG */
@@ -41,6 +42,7 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
