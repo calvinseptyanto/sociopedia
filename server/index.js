578c21385fe7f1731589102c3075a898c118c1b8
calvinseptyanto <98633109+calvinseptyanto@users.noreply.github.com>
2023-09-04 14:35:52 +0800

    Backend config + finished authentication, authorization, & user routes setup

--- server/index.js
+++ server/index.js
@@ -8,6 +8,10 @@ import dotenv from "dotenv";
 import multer from "multer";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import postRoutes from "./routes/posts.js";
+import { register } from "./controllers/auth.js";
+import userRoutes from "./routes/users.js";
 
 /* CONFIG */
 const __filename = fileURLToPath(import.meta.url);
@@ -34,6 +38,14 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+
+/* ROUTES */
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
