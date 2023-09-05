05115a5670bf489684b1bc66e983694d105411f3
calvinseptyanto <98633109+calvinseptyanto@users.noreply.github.com>
2023-09-05 16:54:29 +0800

    Finished register, login pages, and form

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import { themeSettings } from "./theme";
 function App() {
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.user.token));
   return (
     <div className="App">
       <BrowserRouter>
@@ -18,8 +19,14 @@ function App() {
           <CssBaseline />
           <Routes>
             <Route path="/" element={<LoginPage />}></Route>
-            <Route path="/home" element={<HomePage />}></Route>
-            <Route path="/profile/:userId" element={<ProfilePage />}></Route>
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+            ></Route>
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            ></Route>
           </Routes>
         </ThemeProvider>
       </BrowserRouter>
