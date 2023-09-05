faeea24b4486f3aa32df2ada374a4e985fc8ae87
calvinseptyanto <98633109+calvinseptyanto@users.noreply.github.com>
2023-09-05 17:43:38 +0800

    Finished homepage + userwidget

--- client/src/App.js
+++ client/src/App.js
@@ -11,22 +11,23 @@ import { themeSettings } from "./theme";
 function App() {
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
-  const isAuth = Boolean(useSelector((state) => state.user.token));
+  const isAuth = Boolean(useSelector((state) => state.token));
+
   return (
-    <div className="App">
+    <div className="app">
       <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Routes>
-            <Route path="/" element={<LoginPage />}></Route>
+            <Route path="/" element={<LoginPage />} />
             <Route
               path="/home"
               element={isAuth ? <HomePage /> : <Navigate to="/" />}
-            ></Route>
+            />
             <Route
               path="/profile/:userId"
               element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
-            ></Route>
+            />
           </Routes>
         </ThemeProvider>
       </BrowserRouter>
--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -81,7 +81,7 @@ const Form = () => {
   const login = async (values, onSubmitProps) => {
     const loggedInResponse = await fetch("http://localhost:3001/auth/login", {
       method: "POST",
-      header: { "Content-Type": "application/json" },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(values),
     });
     const loggedIn = await loggedInResponse.json();
